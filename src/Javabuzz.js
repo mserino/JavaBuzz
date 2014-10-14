6b09ef4ef7cdb7924c2a39f7592208b37f88643a
mserino <m.serino@hotmail.it>
2014-10-14 15:59:54 +0100

    Refactoring

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,13 +1,18 @@
 function Javabuzz() {
+
+	Javabuzz.prototype._isDivisbleBy = function(number, divisor) {
+		return(number % divisor === 0);
+	};
+
 	Javabuzz.prototype.isDivisibleByThree = function(number) {
-		return (number % 3 === 0);
+		return this._isDivisbleBy(number, 3);
 	};
 
 	Javabuzz.prototype.isDivisibleByFive = function(number) {
-		return (number % 5 === 0);
+		return this._isDivisbleBy(number, 5);
 	};
 
 	Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-		return (number % 15 === 0);
+		return this._isDivisbleBy(number, 15);
 	};
 };
